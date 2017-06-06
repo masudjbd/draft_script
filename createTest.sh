ec73022c5074e5c89a10a980f5728b1d008842a8
Masudur Rahman <masudur.rahman@getzephyr.com>
2017-06-06 14:38:28 -0700

    WeekDay get from date by JS

--- createTest.sh
+++ createTest.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-for i in {1..3000}
+for i in {1..120}
 do
    curl -D- -u admin:admin -X POST --data '{"fields":{"project":{"key":"SAM"},"summary":"Summary sample.","description":"Desc sample.","issuetype":{"name":"Test"}}}' -H "Content-Type: application/json" http://localhost:2990/jira/rest/api/2/issue
 done
---
+++ mixed.js
@@ -0,0 +1,3 @@
+//the day started counting( 1, 2, 3, ...) 
+console.log(new Date('2013-06-07').toLocaleString('en-us',{ weekday: 'long' }));
+// Thursday
