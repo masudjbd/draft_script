9a1329c830e46582ba535ba661277ae0cdba4e07
Masudur Rahman <masudur.rahman@getzephyr.com>
2017-09-08 16:23:18 -0700

    draft jwt test

--- GenerateJwtAuthentication/com/example/demo/JWTSample.java
+++ GenerateJwtAuthentication/com/example/demo/JWTSample.java
@@ -1,25 +1,28 @@
-package sample;
+package com.example.demo;
 
-import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-import com.atlassian.jwt.*;
-import com.atlassian.jwt.core.writer.*;
+import com.atlassian.jwt.SigningAlgorithm;
+import com.atlassian.jwt.core.writer.JsonSmartJwtJsonBuilder;
+import com.atlassian.jwt.core.writer.JwtClaimsBuilder;
+import com.atlassian.jwt.core.writer.NimbusJwtWriterFactory;
 import com.atlassian.jwt.httpclient.CanonicalHttpUriRequest;
 import com.atlassian.jwt.writer.JwtJsonBuilder;
-import com.atlassian.jwt.writer.JwtWriterFactory;
+
+import java.util.HashMap;
 
 public class JWTSample {
 
     public static void main(String[] args)
-            throws UnsupportedEncodingException, NoSuchAlgorithmException {
+          {
+
+              try {
+
         long issuedAt = System.currentTimeMillis() / 1000L;
         long expiresAt = issuedAt + 198880L;
         String key = "com.thed.zephyr.zapi"; //the key from the add-on descriptor
-        String sharedSecret = "secretKey";    //the sharedsecret key received
+        String sharedSecret = "";    //the sharedsecret key received
         //during the add-on installation handshake
         String method = "GET";
-        String baseUrl = "https://client.atlassian.net/";
+        String baseUrl = "https://domain.atlassian.net";
         String contextPath = "/";
         String apiPath = "/rest/atlassian-connect/latest/addons/"+key;
 
@@ -32,12 +35,16 @@ public class JWTSample {
                 apiPath, contextPath, new HashMap());
         JwtClaimsBuilder.appendHttpRequestClaims(jwtBuilder, canonical);
 
-        JwtWriterFactory jwtWriterFactory = new NimbusJwtWriterFactory();
+        NimbusJwtWriterFactory jwtWriterFactory = new NimbusJwtWriterFactory();
         String jwtbuilt = jwtBuilder.build();
         String jwtToken = jwtWriterFactory.macSigningWriter(SigningAlgorithm.HS256,
                 sharedSecret).jsonToJwt(jwtbuilt);
 
         String apiUrl = baseUrl + apiPath + "?jwt=" + jwtToken;
         System.out.println(apiUrl);
+
+              }catch (Exception e){
+
+              }
      }
 }
